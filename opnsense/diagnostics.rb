74d79e1d6f6afa953d31dc26fdcc6c30f6e52dce
Fabian Franz <fabian.franz@students.fh-hagenberg.at>
2016-04-18 15:09:18 +0200

    add some changes

--- opnsense/diagnostics.rb
+++ opnsense/diagnostics.rb
@@ -1,15 +1,42 @@
 require 'uri'
 module OPNsense
-  class Diagnostics
-    class << self  
+  module Diagnostics
+    class << self
       def ndp
         request = OPNsense::Configuration.instance['request']
         request.get_request("diagnostics/interface/getNdp")
       end
-      
+
       def arp
         request = OPNsense::Configuration.instance['request']
         request.get_request("diagnostics/interface/getArp")
+      end
+    end
+    module NetworkInsight
+      class << self
+        # seems to be not working at the moment
+        #def top
+        #  request = OPNsense::Configuration.instance['request']
+        #  request.get_request('diagnostics/networkinsight/top')
+        #end
+        def get_interfaces
+          request = OPNsense::Configuration.instance['request']
+          request.get_request("diagnostics/networkinsight/getinterfaces")
+        end
+        def get_services
+          request = OPNsense::Configuration.instance['request']
+          # keys need to be converted to an integer
+          request.get_request("diagnostics/networkinsight/getservices").map do |key, value|
+            [key.to_i, value]
+          end.to_h
+        end
+        def get_protocols
+          request = OPNsense::Configuration.instance['request']
+          # keys need to be converted to an integer
+          request.get_request("diagnostics/networkinsight/getprotocols").map do |key, value|
+            [key.to_i, value]
+          end.to_h
+        end
       end
     end
   end
